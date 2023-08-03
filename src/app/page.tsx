5e41af1be33a5b54737bb5fb23d68f1fed245cb1
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-08-03 09:32:07 -0300

    feat: add tracking futuristic style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Header from '@/components/shared/organisms/ui-components/header'
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-tl from-spacegray-500 to-spacegray-600 min-h-screen h-full">
+    <main className="bg-gradient-to-tl from-spacegray-500 to-spacegray-600 min-h-screen h-full tracking-widest">
       <Header />
       <LaunchResults />
       <SectionSeparator />
--- src/components/features/launch-registers/molecules/search-form.tsx
+++ src/components/features/launch-registers/molecules/search-form.tsx
@@ -3,9 +3,9 @@ import { MdSearch } from 'react-icons/md'
 const SearchForm = () => {
   return (
     <form className="">
-      <label className="flex border-spaceblue-500 rounded-lg px-4 py-2 border justify-between items-center">
+      <label className="flex border-spaceblue-500 rounded-lg px-4 py-2 border justify-between items-center ">
         <input
-          className="bg-transparent  text-spaceblue-500 placeholder:text-spaceblue-500"
+          className="bg-transparent  text-spaceblue-500 placeholder:text-spaceblue-500 placeholder:tracking-widest"
           type="text"
           name="rocket-search"
           placeholder="PROCURAR"
